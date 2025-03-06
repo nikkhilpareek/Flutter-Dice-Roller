e62eaa623f33e0275811cd3f08995b86f81650b3
Nikhil Pareek <nikhilpareekpandit@gmail.com>
2025-03-06 20:33:52 +0530

    Setting Up Background for Home Page

---
+++ lib/components/gradient_container.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color.fromARGB(255, 216, 203, 117),
+            const Color.fromARGB(255, 64, 70, 72),
+          ],
+        ),
+      ),
+    );
+  }
+}
