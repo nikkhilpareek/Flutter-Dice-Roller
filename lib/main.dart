b2f09d136b80b0cf791c2a87cc55b24b907e77a5
Nikhil Pareek <nikhilpareekpandit@gmail.com>
2025-04-24 01:19:02 +0530

    Setting up github pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:diceroller/components/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,14 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true,
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: Scaffold(
         body: GradientContainer(
           Color.fromARGB(255, 216, 203, 117),
           Color.fromARGB(255, 64, 70, 72),
         ),
       ),
-    ); // TODO: implement build
+    ); 
   }
 }
